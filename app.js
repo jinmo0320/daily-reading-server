0d20dc0f3505240b8764f5d1ea93dca715101d4f
jinmo0320 <jinmo040320@gmail.com>
2022-01-10 00:17:51 +0900

    bug fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const axios = require("axios");
 const cheerio = require("cheerio");
